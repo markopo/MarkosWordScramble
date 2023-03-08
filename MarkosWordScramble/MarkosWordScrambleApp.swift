c30a1ce141d9535cd7bacf718de81058de7a9363
markopedu <69790644+markopedu@users.noreply.github.com>
2023-03-08 11:31:31 +0100

    Initial Commit

---
+++ MarkosWordScramble/MarkosWordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkosWordScrambleApp.swift
+//  MarkosWordScramble
+//
+//  Created by Marko Poikkimäki on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct MarkosWordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
